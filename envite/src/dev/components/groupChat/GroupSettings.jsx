c91dab48307f138e323228e2eb65c348d800d8a4
manjaparidze <anjaparidzemamuka@gmail.com>
2019-03-14 14:30:01 +0400

    Added Group Setting's First Div and it's CSS

--- envite/src/dev/components/groupChat/GroupSettings.jsx
+++ envite/src/dev/components/groupChat/GroupSettings.jsx
@@ -18,7 +18,15 @@ export default class GroupChat extends Component {
                     <button className="groupChatSettings__close"><img className="groupChatSettings__close__icon" src={CloseIcon} alt="" /></button>
                 </div>
                 <div className="groupChatSettings__midSection">
-                    <div></div>
+                    <div className="groupChatSettings__midSection__firstDiv">
+                        <div className="groupChatSettings__midSection__firstDiv__icon">
+                            <span>S</span>
+                        </div>
+                        <span className="groupChatSettings__midSection__firstDiv__text">
+                            It's all about the bag
+                        </span>
+                        <i className="fas fa-chevron-right groupChatSettings__midSection__firstDiv__goTo__icon"></i>
+                    </div>
                     <h2></h2>
                     <div>
                         <div>
@@ -35,7 +43,7 @@ export default class GroupChat extends Component {
                         <div className="groupChatSettings__deleteBtn__icon">
                             <img src={DeleteIcon} alt="" />
                         </div>
-                        <span className="groupChatSettings__deleteBrn__text">Delete Group</span>
+                        <span className="groupChatSettings__deleteBtn__text">Delete Group</span>
                     </button>
                 </div>
             </div>
