6a7e69f6dcf71717dd9ed724c3ec46668b9166b5
manjaparidze <anjaparidzemamuka@gmail.com>
2019-03-15 10:01:42 +0400

    Added eNvite Your Friends Component

--- envite/src/dev/components/groupChat/AddParticipants.jsx
+++ envite/src/dev/components/groupChat/AddParticipants.jsx
@@ -19,23 +19,32 @@ export default function AddParticipants() {
             </div>
             <div className="addParticipantsSettings__midSection">
                 <div className="addParticipants__socialNetwork__wrapper">
-                    <div className="addParticipants__socialNetwork"><i className="fab fa-facebook-f facebook__logo"></i></div>
-                    <span className="addParticipants__socialNetwork__name">Facebook</span>
+                    <div className="addParticipants__socialNetwork">
+                        <i className="fab fa-facebook-f facebook__logo addParticipants__logo logo__border"></i>
+                        <span className="addParticipants__socialNetwork__name">Facebook</span>
+                    </div>
                     <i className="fas fa-chevron-right addParticipants__socialNetwork__goTo"></i>
                 </div>
                 <div className="addParticipants__socialNetwork__wrapper">
-                    <div className="addParticipants__socialNetwork"><i class="fab fa-telegram-plane telegram__logo"></i></div>
-                    <span className="addParticipants__socialNetwork__name">Telegram</span>
+                    <div className="addParticipants__socialNetwork">
+                        <i class="fab fa-telegram-plane telegram__logo addParticipants__logo logo__border"></i>
+                        <span className="addParticipants__socialNetwork__name">Telegram</span>
+                    </div>
                     <i className="fas fa-chevron-right addParticipants__socialNetwork__goTo"></i>
                 </div>
                 <div className="addParticipants__socialNetwork__wrapper">
-                    <div className="addParticipants__socialNetwork"><i class="fab fa-whatsapp whatsApp__logo"></i></div>
-                    <span className="addParticipants__socialNetwork__name">WhatsApp</span>
+                    <div className="addParticipants__socialNetwork">
+                        <i class="fab fa-whatsapp whatsApp__logo addParticipants__logo logo__border"></i>
+                        <span className="addParticipants__socialNetwork__name">WhatsApp</span>
+                    </div>
                     <i className="fas fa-chevron-right addParticipants__socialNetwork__goTo"></i>
                 </div>
                 <div className="addParticipants__socialNetwork__wrapper">
-                    <div className="addParticipants__socialNetwork"><i class="fas fa-envelope email__logo"></i></div>
-                    <span className="addParticipants__socialNetwork__name">Email</span>
+                    <div className="addParticipants__socialNetwork">
+                        <i class="fas fa-envelope email__logo addParticipants__logo logo__border"></i>
+
+                        <span className="addParticipants__socialNetwork__name">Email</span>
+                    </div>
                     <i className="fas fa-chevron-right addParticipants__socialNetwork__goTo"></i>
                 </div>
             </div>
